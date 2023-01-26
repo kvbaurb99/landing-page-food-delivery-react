67cee00e39e6ebc4b4679e9dc4a9af3acbd7b806
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-01-26 19:40:58 +0100

    footer added to main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,8 @@ import NavBar from './NavBar'
 import Restaurants from './Restaurants'
 import Foods from './Foods'
 import FoodsContainer from './FoodsContainer'
+import Informations from './Informations'
+import Footer from './Footer'
 
 
 export default function Main({products, asian, burgers, all, pizza, european, low, lowest, high, highest}) {
@@ -12,6 +14,8 @@ export default function Main({products, asian, burgers, all, pizza, european, lo
         <Restaurants />
         <Foods asian={asian} burgers={burgers} all={all} pizza={pizza} european={european} low={low} high={high} lowest={lowest} highest={highest} />
         <FoodsContainer products={products} />
+        <Informations />
+        <Footer />
     </>
   )
 }
