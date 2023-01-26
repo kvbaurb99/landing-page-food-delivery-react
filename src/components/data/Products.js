82f3785282c7d9714a49a96694386c4a424f858d
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-01-26 17:30:01 +0100

    array of products

---
+++ src/components/data/Products.js
@@ -0,0 +1,100 @@
+const productsArray = [
+    {   
+        name: 'Double Cheeseburger',
+        img: 'https://cdn.concreteplayground.com/content/uploads/2021/08/Goldieboy-kit-burger-supplied.jpg',
+        price: '$$$',
+        type: 'burger'
+    },
+    {
+        name: 'Sushi Hanzo Set',
+        img: 'https://s1.1zoom.me/b4853/148/Seafoods_Sushi_Black_background_529723_1920x1080.jpg',
+        price: '$$$$',
+        type: 'asian'
+    },
+    {
+        name: 'Pad Thai',
+        img: 'https://img.freepik.com/premium-photo/traditional-dish-pad-thai-with-shrimp-dark-stone-background-top-view_233226-715.jpg',
+        price: '$$',
+        type: 'asian'
+    },
+    {
+        name: 'Pepperoni Pizza',
+        img: 'https://img.freepik.com/premium-zdjecie/smakowita-pepperoni-pizza-i-kulinarni-skladniki_79782-15.jpg?w=2000',
+        price: '$$',
+        type: 'pizza'
+    },
+    {
+        name: 'Bacon Cheeseburger',
+        img: 'https://popmenucloud.com/cdn-cgi/image/width=1920,height=1920,format=auto,fit=scale-down/zovhgdjk/49bea355-0324-405f-bea8-8a0b0f4a34a2.jpg',
+        price: '$$$',
+        type: 'burger'
+    },
+    {
+        name: 'Fish Curry',
+        img: 'http://cdn.shopify.com/s/files/1/0524/2113/2440/articles/fish_curry.jpg?v=1664800926',
+        price: '$',
+        type: 'asian'
+    },
+    {
+        name: 'Porterhouse Steak',
+        img: 'https://www.winemag.com/wp-content/uploads/2020/12/Pairings_Luxe_Pairings_Porterhouse_Credit_Kate_Sears_1920x1280.jpg',
+        price: '$$$$',
+        type: 'european'
+    },
+    {
+        name: 'Pizza Capricciosa',
+        img: 'https://c1.wallpaperflare.com/preview/945/713/318/pizza-marherita-capriciosa.jpg',
+        price: '$$',
+        type: 'pizza'
+    },
+    {
+        name: 'Spaghetti Carbonara',
+        img: 'https://britishop.com/storage/imgcache/carbonara-pasta-spaghetti-alla-carbonara-P8ZSPDE__1000x600xauto.jpg',
+        price: '$$',
+        type: 'european'
+    },
+    {
+        name: 'Shoyu Ramen',
+        img: 'https://azjanatalerzu.pl/data/include/img/news/1605786150.jpg',
+        price: '$$$',
+        type: 'asian'
+    },
+    {
+        name: 'Fish and Chips',
+        img: 'https://www.thespruceeats.com/thmb/T9WD6Ea90-ugl5dzff-OFlkbw0Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/IMG_5442-58b427d35f9b586046d8dfa1.JPG',
+        price: '$',
+        type: 'european'
+    },
+    {
+        name: 'Vege Burger',
+        img: 'http://kraftmagia.pl/wp-content/uploads/2020/01/karta_12.2019-72-1024x683.jpg',
+        price: '$$$',
+        type: 'burger'
+    },
+    {
+        name: 'Pizza Margherita',
+        img: 'https://www.foodandwine.com/thmb/3kzG4PWOAgZIIfZwMBLKqoTkaGQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/margherita-pizza-with-argula-and-prosciutto-FT-RECIPE0721-04368ec288a84d2e997573aca0001d98.jpg',
+        price: '$$',
+        type: 'pizza'
+    },
+    {
+        name: 'Khachapuri',
+        img: 'https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2021-02/khachapuri.jpg?itok=PGjSYSJk',
+        price: '$',
+        type: 'european'
+    },
+    {
+        name: 'Neapolitan Pizza',
+        img: 'https://tastyoven.com/wp-content/uploads/2022/01/neapolitan-pizza-dough-image-featured.jpg',
+        price: '$$',
+        type: 'pizza'
+    },
+    {
+        name: 'Jalapeno Burger',
+        img: 'https://api.specfood.pl/assets/media/burger-jalapeno_aboutUsThumb.jpg',
+        price: '$$$',
+        type: 'burger'
+    }
+]
+
+export default productsArray
