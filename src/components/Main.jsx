9ee5adb4f8972d174105bb7896abb7601851e017
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-01-26 12:19:21 +0100

    main page

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Restaurants from './Restaurants'
+
+
+export default function Main() {
+  return (
+    <>
+        <NavBar />
+        <Restaurants />
+    </>
+  )
+}
