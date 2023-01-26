f636a585a3281192f3b995a40c3186172affeb20
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-01-26 17:29:45 +0100

    import props

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import NavBar from './NavBar'
 import Restaurants from './Restaurants'
 import Foods from './Foods'
+import FoodsContainer from './FoodsContainer'
 
 
-export default function Main() {
+export default function Main({products, asian, burgers, all, pizza, european, low, lowest, high, highest}) {
   return (
     <>
         <NavBar />
         <Restaurants />
-        <Foods />
+        <Foods asian={asian} burgers={burgers} all={all} pizza={pizza} european={european} low={low} high={high} lowest={lowest} highest={highest} />
+        <FoodsContainer products={products} />
     </>
   )
 }
